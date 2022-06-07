c1c73548a3f980d8e4a415a63db22997760ac198
Madeline Mauser <madeline.mauser@gmail.com>
2022-06-07 16:30:39 -0600

    Add three boolean variables

--- variables.rb
+++ variables.rb
@@ -3,9 +3,13 @@ first_name = "Madeline"
 birth_month = "November"
 
 shrimp = 4
-dogs = 7
-cats = 1
+dogs = 1
+cats = 0
 
 gas_cost = 60.70
 in_wallet = 24.25
 in_bank = 1205.07
+
+awake = true
+hungry = true
+sleepy = false
